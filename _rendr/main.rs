ad0aef8293d255337f3d00880fb2e1d8120e0f2b
Mateusz Czapliński <czapkofan@gmail.com>
2023-09-01 14:28:36 +0200

    _rendr: sketch code TODOs

--- _rendr/main.rs
+++ _rendr/main.rs
@@ -1,3 +1,9 @@
 fn main() {
     println!("hello rendr!");
+
+    // TODO: load sources: *.md & _drafts/*.md
+    // TODO: parse filename (slug, tags, date, file extension)
+    // -< PEG or parser-combinator, ideally with DSL/macro/annotation
+    // TODO: render articles
+    // TODO: render list
 }
