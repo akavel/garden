eecf7be62d6b14a6fafb343b38947742209d0e5d
Mateusz Czapliński <czapkofan@gmail.com>
2024-01-11 09:16:58 +0100

    (cargo fmt)

--- _rendr/main.rs
+++ _rendr/main.rs
@@ -29,7 +29,8 @@ use html::Html;
 use pathinfo::PathInfo;
 
 const SOURCES: &str = "*.md:@seed/*.md";
-const RAW: &str = "favicon.ico:*.pdf:@seed/*.pdf:*.png:@seed/*.png:*.jpg:@seed/*.jpg:*.svg:@seed/*.svg";
+const RAW: &str =
+    "favicon.ico:*.pdf:@seed/*.pdf:*.png:@seed/*.png:*.jpg:@seed/*.jpg:*.svg:@seed/*.svg";
 const SCRIPT_PATH: &str = "_bloat/bloat.lua";
 const OUT_DIR: &str = "_html.out";
 
